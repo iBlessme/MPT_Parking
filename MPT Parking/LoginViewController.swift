4a7ea39559295ee259132bf47de3f51c04cb95b1
Vladislav Legavin <vldikk12@gmail.com>
2021-03-10 09:59:09 +0300

    demo

---
+++ MPT Parking/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  MPT Parking
+//
+//  Created by Blessme on 18.02.2021.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    var window: UIWindow?
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    func showNewWindow(){
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "mainView") as UIViewController
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func signIn(_ sender: UIButton) {
+        
+        Auth.auth().signIn(withEmail: loginTextField.text!, password: passwordTextField.text!) {(result, error) in
+            if error == nil{
+                self.showNewWindow()
+               
+                
+       
+            }
+            
+        }
+    }
+    
+
+}
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MPT Parking' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MPT Parking
+	pod 'Firebase/Analytics'
+	pod 'Firebase/Firestore'
+	pod 'Firebase/Auth'
+	pod 'Firebase/Database'
+	pod 'Firebase'
+
+
+  target 'MPT ParkingTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MPT ParkingUITests' do
+    # Pods for testing
+  end
+
+end
